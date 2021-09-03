b8b0b62d8aa247bed4df3da83c5f7beab4f304bf
Manoj Motapalukula <89991377+manojmotapalukula@users.noreply.github.com>
2021-09-04 01:05:23 +0530

    new file uploaded

---
+++ letsassign2.py
@@ -0,0 +1,26 @@
+#program to find prime or not
+
+
+num=int(input("enter a number"))
+for i in range (2,num):
+    if num % i ==0:
+        print(f"{num} is Not a prime number")
+        break
+else:
+    print(f"{num} is a prime number")
+
+#using flag
+    
+num=int(input("enter a number"))
+flag=0
+for i in range (2,num):
+    if num % i ==0:
+        print(f"{num} is Not a prime number")
+        flag=1
+        break
+if flag==0:
+    print(f"{num} is a prime number")
+
+
+
+
